29f4d16fdf50375f68a1eb1209aeed85d4f46ab9
Kevin Bertrand <73759450+kevin-bertrand@users.noreply.github.com>
2023-01-12 12:12:35 +0100

    Adding alert view with text field

--- Sources/Desyntic_Library/View/AlertView.swift
+++ Sources/Desyntic_Library/View/AlertView.swift
@@ -9,6 +9,34 @@ import Foundation
 import SwiftUI
 
 extension View {
+    /// Showing alert view with text field
+    func alertWithTextField(title: String,
+                            message: String,
+                            hintText: String,
+                            primaryTitle: String,
+                            secondaryTitle: String,
+                            primaryAction: @escaping (String) -> Void,
+                            secondaryAction: @escaping () -> Void) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addTextField { field in
+            field.placeholder = hintText
+        }
+        
+        alert.addAction(.init(title: secondaryTitle, style: .cancel, handler: { _ in
+            secondaryAction()
+        }))
+        
+        alert.addAction(.init(title: primaryTitle, style: .default, handler: { _ in
+            if let text = alert.textFields?[0].text {
+                primaryAction(text)
+            } else {
+                primaryAction("")
+            }
+        }))
+        
+        rootController().present(alert, animated: true, completion: nil)
+    }
+    
     /// Showing standard alert view
     func alertView(title: String,
                    message: String,
