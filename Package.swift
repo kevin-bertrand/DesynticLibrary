c8d5730a4273a72712cc5d94fdbcf6d536ce49f3
Kevin Bertrand <73759450+kevin-bertrand@users.noreply.github.com>
2023-01-13 08:08:08 +0100

    Update Library name

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Desyntic_Library",
+    name: "DesynticLibrary",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Desyntic_Library",
-            targets: ["Desyntic_Library"]),
+            name: "DesynticLibrary",
+            targets: ["DesynticLibrary"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Desyntic_Library",
+            name: "DesynticLibrary",
             dependencies: []),
         .testTarget(
-            name: "Desyntic_Library_Tests",
-            dependencies: ["Desyntic_Library"]),
+            name: "DesynticLibraryTests",
+            dependencies: ["DesynticLibrary"]),
     ]
 )
--- Tests/DesynticLibraryTests/DesynticLibraryTests.swift
+++ Tests/DesynticLibraryTests/DesynticLibraryTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import Desyntic_Library
+@testable import DesynticLibrary
 
-final class Desyntic_Library_Tests: XCTestCase {
+final class DesynticLibraryTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
