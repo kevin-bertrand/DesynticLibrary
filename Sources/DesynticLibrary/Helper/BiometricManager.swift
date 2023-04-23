5ba521d2d9317213ca52378eecdba153a0899e8d
Kevin Bertrand <kevin.bertrand@digimobee.com>
2023-04-23 20:51:03 +0200

    Update login view

--- Sources/DesynticLibrary/Helper/BiometricManager.swift
+++ Sources/DesynticLibrary/Helper/BiometricManager.swift
@@ -8,17 +8,30 @@
 import Foundation
 import LocalAuthentication
 
-public class BiometricManager {
-    static public func activateBiometric(message: String, completionHandler: @escaping (Bool) -> Void) {
+public class BiometricManager: ObservableObject {
+    // MARK: Static methods
+    static private(set) var laContext = LAContext()
+    static public func useBiometrics(message: String, password: String? = nil, completionHandler: @escaping (Bool) -> Void) {
         var error: NSError?
-        let laContext = LAContext()
         
-        if laContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+        if BiometricManager.laContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
             let reason = message
             
-            laContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, error in
+            BiometricManager.laContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, error in
+                if let password,
+                   success  {
+                    BiometricManager.laContext.setCredential(password.data(using: .utf8), type: .applicationPassword)
+                }
                 completionHandler(success)
             }
         }
+    }
+    
+    static var isActive: Bool {
+        laContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: .none)
+    }
+    
+    static public var isBiometricsSet: Bool {
+        laContext.isCredentialSet(.applicationPassword)
     }
 }
--- Sources/DesynticLibrary/View/Biometrics.swift
+++ Sources/DesynticLibrary/View/Biometrics.swift
@@ -5,49 +5,43 @@
 //  Created by Kevin Bertrand on 13/01/2023.
 //
 
-import LocalAuthentication
 import SwiftUI
 
 public struct BiometricView: View {
     // MARK: State properties
-    @State private var isAvailable: Bool = false
+    @StateObject private var biometricManager: BiometricManager
     
     // MARK: Binding properties
     @Binding var condition: Bool
     
     // MARK: Properties
-    private let laContext = LAContext()
     var action: () -> Void
     
     // MARK: Initialization
     public init(condition: Binding<Bool> = .constant(true),
                 action: @escaping () -> Void) {
+        self._biometricManager = StateObject(wrappedValue: BiometricManager())
         self._condition = condition
         self.action = action
-        self.isAvailable = laContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: .none)
+        print("Is active: \(BiometricManager.isActive)")
+        print("Condition: \(condition)")
     }
     
     // MARK: Body
     public var body: some View {
         Group {
-            if isAvailable && condition  {
+            if BiometricManager.isActive && condition  {
                 VStack {
                     Spacer()
                     
                     Button {
                         action()
                     } label: {
-                        Image(systemName: (laContext.biometryType == .faceID) ? "faceid" : "touchid")
+                        Image(systemName: (BiometricManager.laContext.biometryType == .faceID) ? "faceid" : "touchid")
                             .resizable()
                             .frame(width: 75, height: 75)
                     }
                     .padding(.horizontal)
-                    .onAppear {
-                        withAnimation {
-                            isAvailable = laContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
-                                                                      error: .none)
-                        }
-                    }
                 }
             } else {
                 EmptyView()
